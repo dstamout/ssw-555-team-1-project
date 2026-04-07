35b86b5c14384f0b1449dab56d20d36975cca831
Krupabhagat11 <kbhagat2@stevens.edu>
2026-04-06 23:22:51 -0400

    User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const { Schema } = mongoose;
 
@@ -12,5 +13,26 @@ const UserSchema = new Schema({
   pushTokens: [{ type: String }],
   createdAt: { type: Date, default: Date.now }
 });
+
+// Pre-save hook to hash password if it was modified
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // skip if password not changed
+
+  if (this.password) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (err) {
+      return next(err);
+    }
+  }
+  next();
+});
+
+// Method to compare password during login
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  if (!this.password) return false; // for Google OAuth users
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 export default mongoose.models.User || mongoose.model('User', UserSchema);
